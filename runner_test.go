c94f15a8d4f99926109b138950d74cd2973b4c2b
Nick Poorman <npoorman@redventures.com>
2019-01-22 16:31:51 -0500

    Implemented weighted queues

--- runner_test.go
+++ runner_test.go
@@ -1,6 +1,7 @@
 package faktory_worker
 
 import (
+	"math/rand"
 	"testing"
 
 	faktory "github.com/contribsys/faktory/client"
@@ -24,4 +25,23 @@ func TestContext(t *testing.T) {
 	assert.Equal(t, ctx.Jid(), job.Jid)
 	_, ok := ctx.Deadline()
 	assert.False(t, ok)
+}
+
+func TestWeightedQueues(t *testing.T) {
+	t.Parallel()
+	rand.Seed(42)
+	mgr := NewManager()
+	mgr.Queues = []string{"critical", "default", "bulk"}
+	mgr.UseWeightedQueues([]int{3, 2, 1})
+	assert.Equal(t, []string{"critical", "default", "bulk"}, mgr.getQueues())
+	assert.Equal(t, []string{"critical", "bulk", "default"}, mgr.getQueues())
+
+	mgr.UseWeightedQueues([]int{1, 100, 1000})
+	assert.Equal(t, []string{"bulk", "default", "critical"}, mgr.getQueues())
+
+	mgr.UseWeightedQueues([]int{1, 1000, 100})
+	assert.Equal(t, []string{"default", "bulk", "critical"}, mgr.getQueues())
+
+	mgr.UseWeightedQueues([]int{1, 1, 1})
+	assert.Equal(t, []string{"default", "critical", "bulk"}, mgr.getQueues())
 }
