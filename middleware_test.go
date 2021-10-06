38a9266682ef0ae1b3d1b2afc09d650f2350cc29
Mike Perham <mperham@gmail.com>
2021-10-06 13:47:12 -0700

    silence linter

--- middleware_test.go
+++ middleware_test.go
@@ -8,6 +8,10 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+type middlewareValue string
+
+var EXAMPLE = middlewareValue("a")
+
 func TestMiddleware(t *testing.T) {
 	mgr := NewManager()
 	pool, err := faktory.NewPool(2)
@@ -15,13 +19,13 @@ func TestMiddleware(t *testing.T) {
 	mgr.Pool = pool
 
 	mgr.Use(func(ctx context.Context, job *faktory.Job, next func(context.Context) error) error {
-		modctx := context.WithValue(ctx, "a", 4.0)
+		modctx := context.WithValue(ctx, EXAMPLE, 4.0)
 		return next(modctx)
 	})
 
 	counter := 0
 	blahFunc := func(ctx context.Context, job *faktory.Job) error {
-		assert.EqualValues(t, 4.0, ctx.Value("a"))
+		assert.EqualValues(t, 4.0, ctx.Value(EXAMPLE))
 		help := HelperFor(ctx)
 		assert.Equal(t, job.Jid, help.Jid())
 		assert.Equal(t, job.Type, help.JobType())
@@ -32,13 +36,13 @@ func TestMiddleware(t *testing.T) {
 
 	job := faktory.NewJob("blah", 1, 2)
 	ctx := jobContext(mgr.Pool, job)
-	assert.Nil(t, ctx.Value("a"))
+	assert.Nil(t, ctx.Value(EXAMPLE))
 	assert.EqualValues(t, 0, counter)
 
 	err = dispatch(mgr.middleware, ctx, job, blahFunc)
 
 	assert.NoError(t, err)
 	assert.EqualValues(t, 1, counter)
-	assert.Nil(t, ctx.Value("a"))
+	assert.Nil(t, ctx.Value(EXAMPLE))
 
 }
