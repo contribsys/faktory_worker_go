6f2991fa6272e16feab55e3dde5d39130cbe8a58
Felix Kollmann <mail@fkollmann.de>
2022-04-24 14:31:33 +0200

    Move retrieving batch ID from callback job to separate method

--- context.go
+++ context.go
@@ -45,6 +45,10 @@ type Helper interface {
 	Bid() string
 
 	// Faktory Enterprise:
+	// the BID of the Batch associated with this callback (complete or success) job
+	CallbackBid() string
+
+	// Faktory Enterprise:
 	// open the batch associated with this job so we can add more jobs to it.
 	//
 	//   func myJob(ctx context.Context, args ...interface{}) error {
@@ -80,7 +84,10 @@ func (h *jobHelper) Bid() string {
 	if b, ok := h.job.GetCustom("bid"); ok {
 		return b.(string)
 	}
-	if b, ok := h.job.GetCustom("_bid"); ok { // for batch complete jobs
+	return ""
+}
+func (h *jobHelper) CallbackBid() string {
+	if b, ok := h.job.GetCustom("_bid"); ok {
 		return b.(string)
 	}
 	return ""
